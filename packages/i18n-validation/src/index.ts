2e061dc9925092de3a42a26cbbefbd40685eeaac
Hanfour.Huang <hanfourhuang@guoshipartners.com>
2026-05-12 11:57:46 +0800

    feat(i18n-validation): setGlobalLocaleErrorMap + public exports

--- packages/i18n-validation/src/index.ts
+++ packages/i18n-validation/src/index.ts
@@ -1,1 +1,21 @@
-export {};
+export {
+  LOCALES,
+  DEFAULT_LOCALE,
+  LOCALE_COOKIE,
+  isLocale,
+  pickFromAcceptLanguage,
+  resolveLocale,
+  type Locale,
+  type ResolveLocaleInput,
+} from "./locales.js";
+
+export {
+  loadValidationMessages,
+  type ValidationMessages,
+} from "./messages.js";
+
+export { createErrorMap } from "./errorMap.js";
+
+export { runWithLocale, currentLocale } from "./runtime.js";
+
+export { setGlobalLocaleErrorMap } from "./setup.js";
